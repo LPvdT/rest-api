ef4a877a4c126e449f2184b1c9a5c0a4e531b763
Laurens van der Tas <laurens.vandertas@gmail.com>
2024-02-24 10:37:11 +0100

    update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,12 @@
-use actix_web::{get, web, App, HttpResponse, HttpServer};
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use serde::{Deserialize, Serialize};
 use std::sync::Mutex;
+
 mod todolist;
-
-#[allow(dead_code)]
-pub(self) struct AppState {
-    todolist_entries: Mutex<Vec<TodoListEntry>>,
-}
-
-#[derive(Debug, Serialize, Deserialize, Clone)]
-pub(self) struct TodoListEntry {
-    id: i32,
-    date: i64,
-    title: String,
-}
-
-#[get("/")]
-pub(crate) async fn index() -> String {
-    let r = HttpResponse::Ok().finish().status();
-    format!("{}: This is a health check.", r)
-}
+use todolist::services;
 
 #[actix_web::main]
-pub(crate) async fn main() -> std::io::Result<()> {
+async fn main() -> std::io::Result<()> {
     let app_data = web::Data::new(AppState {
         todolist_entries: Mutex::new(vec![]),
     });
@@ -31,8 +15,36 @@ pub(crate) async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(app_data.clone())
             .service(index)
+            .configure(services::config)
     })
     .bind(("127.0.0.1", 8080));
 
     server?.run().await
+}
+
+#[get("/")]
+async fn index() -> impl Responder {
+    let res = HttpResponse::Ok().finish().status();
+
+    HttpResponse::Ok().json(HealthCheckDto {
+        code: res.into(),
+        msg: "This is a health check".to_string(),
+    })
+}
+
+pub struct AppState {
+    todolist_entries: Mutex<Vec<TodoListEntryDto>>,
+}
+
+#[derive(Debug, Serialize, Deserialize, Clone)]
+pub struct TodoListEntryDto {
+    id: i32,
+    date: i64,
+    title: String,
+}
+
+#[derive(Serialize)]
+pub struct HealthCheckDto {
+    code: u16,
+    msg: String,
 }
